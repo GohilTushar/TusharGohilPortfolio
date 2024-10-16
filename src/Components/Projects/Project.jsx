0f7a5959648221275c8e039a0b50b23873ca5a34
Tushar Gohil <gohiltushar3012@gmail.com>
2024-10-16 22:47:36 +0530

    update the projects

--- src/Components/Projects/Project.jsx
+++ src/Components/Projects/Project.jsx
@@ -1,21 +1,18 @@
 import React from "react";
 import "./Project.css";
-import { FaReact, FaHardHat } from "react-icons/fa";
 import ExpenseTracker from "../../Assets/ExpenseTracker.png";
 import PassWordGenerator from "../../Assets/PassWordGenerator.png";
 import EyeMover from "../../Assets/EyeMover.png";
-import Restaurant from "../../Assets/Restaurant.png";
+import LMS from "../../Assets/LMS.png"
+import IG from "../../Assets/IG.png"
 import {
   SiHtml5,
   SiExpress,
-  SiTailwindcss,
-  SiReactrouter,
-  SiJavascript,
-  SiMongodb,
   SiNodedotjs,
+  SiReact,
+  SiMysql,
 } from "react-icons/si";
 import { DiCss3 } from "react-icons/di";
-import { VscJson } from "react-icons/vsc";
 import { IoLogoJavascript } from "react-icons/io";
 export const Project = () => {
   return (
@@ -23,6 +20,94 @@ export const Project = () => {
       <div className="section main">
         <h2 className="section__title different">Projects</h2>
         <div className="allProjects">
+          <div className="projects_container" data-aos="fade-right">
+            <div className="project">
+              <div className="project_videocontainer">
+                <div>
+                  <img
+                    src={IG}
+                    alt="Invoice Generator"
+                  />
+                </div>
+              </div>
+              <div className="project_information">
+                <h2>Invoice Generator</h2>
+                <p>
+                  Allowing users to create, manage, and track invoices with CRUD operations. Integrated pagination, search, filtering, and sorting features for efficient invoice management
+                </p>
+                <div>
+                  <SiMysql />
+                  <SiExpress />
+                  <SiReact />
+                  <SiNodedotjs />
+                </div>
+                <div>
+                  <a
+                    href="https://github.com/GohilTushar/Invoice-Generator"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    <span type="button" className="btns onbt">
+                      See this Live
+                    </span>
+                  </a>
+                  <a
+                    href="https://github.com/GohilTushar/Invoice-Generator"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    <span type="button" className="btns onbt">
+                      View Code
+                    </span>
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="projects_container" data-aos="fade-right">
+            <div className="project">
+              <div className="project_videocontainer">
+                <div>
+                  <img
+                    src={LMS}
+                    alt="Leave Management Syatem"
+                  />
+                </div>
+              </div>
+              <div className="project_information">
+                <h2>LMS</h2>
+                <p>
+                  a MERN-based Leave Management System (LMS) with employee leave requests, approvals, and real-time tracking, streamlining the leave process with automated notifications
+                </p>
+                <div>
+                  <SiMysql />
+                  <SiExpress />
+                  <SiReact />
+                  <SiNodedotjs />
+                </div>
+                <div>
+                  <a
+                    href="https://github.com/GohilTushar/LMS"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    <span type="button" className="btns onbt onbt">
+                      See this Live
+                    </span>
+                  </a>
+                  <a
+                    href="https://github.com/GohilTushar/LMS"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    <span type="button" className="btns onbt">
+                      View Code
+                    </span>
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
           <div className="projects_container" data-aos="fade-right">
             <div className="project">
               <div className="project_videocontainer">
@@ -36,13 +121,13 @@ export const Project = () => {
               <div className="project_information">
                 <h2>Expense Tracker </h2>
                 <p>
-                  It provides a systematic way to record, categorize, and analyze expenses, allowing users to gain insights into their financial habits, make informed decisions, and create effective budgets. 
+                  It provides a systematic way to record, categorize, and analyze expenses, allowing users to gain insights into their financial habits, make informed decisions, and create effective budgets.
                 </p>
                 <div>
-                  <SiHtml5/>
-                  <DiCss3/>
-                  <IoLogoJavascript/>
-                  <FaReact />
+                  <SiHtml5 />
+                  <DiCss3 />
+                  <IoLogoJavascript />
+                  <SiReact />
                 </div>
                 <div>
                   <a
@@ -83,10 +168,10 @@ export const Project = () => {
                   It Is Difficult to Think About Strong Password For Security Purpose But With The Help Of This Interface Anyone Can Easily Generate Password Of Any Length And Combination Of Any Character
                 </p>
                 <div>
-                  <SiHtml5/>
-                  <DiCss3/>
-                  <IoLogoJavascript/>
-                  <FaReact />
+                  <SiHtml5 />
+                  <DiCss3 />
+                  <IoLogoJavascript />
+                  <SiReact />
                 </div>
                 <div>
                   <a
@@ -100,94 +185,6 @@ export const Project = () => {
                   </a>
                   <a
                     href="https://github.com/GohilTushar/PasswordGenerator"
-                    target="_blank"
-                    rel="noreferrer"
-                  >
-                    <span type="button" className="btns onbt">
-                      View Code
-                    </span>
-                  </a>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="projects_container" data-aos="fade-right">
-            <div className="project">
-              <div className="project_videocontainer">
-                <div>
-                  <img
-                    src={EyeMover}
-                    alt="Eye Mover"
-                  />
-                </div>
-              </div>
-              <div className="project_information">
-                <h2>EyeMover</h2>
-                <p>
-                  With The Movement Of Cursor Or Mouse Pointer Pupils Of Animated Eye Also Move and Basic Use Of ClientX And ClientY
-                </p>
-                <div>
-                  <DiCss3 />
-                  <SiHtml5 />
-                  <IoLogoJavascript />
-                </div>
-                <div>
-                  <a
-                    href="https://gohiltushar.github.io/EyeMover/"
-                    target="_blank"
-                    rel="noreferrer"
-                  >
-                    <span type="button" className="btns onbt">
-                      See this Live
-                    </span>
-                  </a>
-                  <a
-                    href="https://github.com/GohilTushar/EyeMover"
-                    target="_blank"
-                    rel="noreferrer"
-                  >
-                    <span type="button" className="btns onbt">
-                      View Code
-                    </span>
-                  </a>
-                </div>
-              </div>
-            </div>
-          </div>
-
-          <div className="projects_container" data-aos="fade-right">
-            <div className="project">
-              <div className="project_videocontainer">
-                <div>
-                  <img
-                    src={Restaurant}
-                    alt="PulsePlus-Clone"
-                  />
-                </div>
-              </div>
-              <div className="project_information">
-                <h2>Restaurant Menu</h2>
-                <p>
-                  This is The Dynamic Content Filter project that allows users to dynamically filter and display content based on specified criteria. 
-
-                </p>
-                <div>
-                  <DiCss3 />
-                  <SiHtml5 />
-                  <IoLogoJavascript />
-                </div>
-                <div>
-                  <a
-                    href="https://gohiltushar.github.io/Restaurant/"
-                    target="_blank"
-                    rel="noreferrer"
-                  >
-                    <span type="button" className="btns onbt onbt">
-                      See this Live
-                    </span>
-                  </a>
-                  <a
-                    href="https://github.com/GohilTushar/Restaurant"
                     target="_blank"
                     rel="noreferrer"
                   >
